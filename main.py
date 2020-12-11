b410526a2b48d3780a4e10ea17ca54c39de7668d
Shweta Burgula <burgulashweta@gmail.com>
2020-12-11 03:39:56 +0000

    Day 3 Solution

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ dataFile3 = open("day3data.txt")
 d3 = dataFile3.readlines()
 dataFile3.close()
 
-ans1 = 0
+def treeFinder (right, down):
+  ans = 0
+  pos = 0
 
-pos = 0
-for i in d3:
-  if i[pos] == "#":
-    ans1 = ans1 + 1 #that means we hit a tree
-  pos = (pos + 3) % (len(i) - 1)
+  for i in range(0, len(d3), down):
+    if (d3[i])[pos] == "#":
+      ans = ans + 1 
+    pos = (pos + right) % (len(d3[i]) - 1)
+  
+  return ans
 
-print ("Day 3 Part 1: ", str(ans1))
+print ("Day 3 Part 1: ", str(treeFinder(3, 1)))
+
+#Part 2
+ans2 = treeFinder(1,1) * treeFinder(3,1) * treeFinder(5,1) * treeFinder(7,1) * treeFinder(1, 2)
+
+print ("Day 3 Part 2: ", str(ans2))
+
